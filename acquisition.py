5fa4a53f35fd8ba32ffa719d2e2bc277267cad2f
hefadog2 <>
2021-11-02 18:24:30 -0400

    First upload of acquisition

---
+++ acquisition.py
@@ -0,0 +1,41 @@
+import spotipy
+sp = spotipy.Spotify()
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
+from time import sleep
+
+# setting up authorization
+cid ="" 
+secret = ""
+
+username = 'your_account_number'
+scope = 'user-library-read,user-read-playback-state,user-modify-playback-state' #check the documentation
+authorization_url = 'https://accounts.spotify.com/authorize'
+token_url = 'https://accounts.spotify.com/api/token'
+redirect_uri =''
+
+token = util.prompt_for_user_token(username,scope,client_id='',client_secret='',redirect_uri='')
+
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+analysis = sp.audio_analysis("https://open.spotify.com/track/3Yb3Z1HRrMrBh886cayujG?si=89aa3cca4e6c4f5f")
+features = sp.audio_features("https://open.spotify.com/track/3Yb3Z1HRrMrBh886cayujG?si=89aa3cca4e6c4f5f")
+features_df = pd.DataFrame(data=features, columns=features[0].keys())
+beats_df = pd.DataFrame(data=analysis['beats'])
+segments_df = pd.DataFrame(data=analysis['segments'])
+bars_df = pd.DataFrame(data=analysis['bars'])
+tatums_df = pd.DataFrame(data=analysis['tatums'])
+
+
+plt.figure(figsize=(20,30))
+sns.pairplot(beats_df)
+plt.xticks(rotation=90)
+
+print(segments_df)
