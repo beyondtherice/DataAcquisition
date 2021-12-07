17bd7f3e4562023b06c026ffe018f4939e749612
hefadog2 <>
2021-12-07 17:57:02 -0500

    Added functionalization to acquisition.py and apped an empty .osu file to be tested on (delete all hit markers hin file after a test to restore its original state)

--- dataacquisition/acquisition.py
+++ dataacquisition/acquisition.py
@@ -1,39 +1,47 @@
 """analyzes given spotify song"""
-import os
-
-import matplotlib.pyplot as plt
-import pandas as pd
-import spotipy
-from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyClientCredentials
-
-# environment variables
-load_dotenv()
+mport spotipy
 
 sp = spotipy.Spotify()
+from pprint import pprint
+from time import sleep
 
-cid = os.environ.get("CLIENT_ID")
-secret = os.environ.get("CLIENT_SECRET")
 
-SCOPE = "user-library-read,user-read-playback-state,user-modify-playback-state"
-AUTHORIZATION_URL = "https://accounts.spotify.com/authorize"
-TOKEN_URL = "https://accounts.spotify.com/api/token" # nosec
+import pandas as pd
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
-client_credentials_manager = SpotifyClientCredentials(
-    client_id=cid, client_secret=secret
-)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+def authentication(cid,secret,username):    
+    scope = "user-library-read,user-read-playback-state,user-modify-playback-state"
+    authorization_url = "https://accounts.spotify.com/authorize"
+    token_url = "https://accounts.spotify.com/api/token"
+    redirect_uri = uri
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+                    
+    return sp
 
-# in quotes should be track uri, i.e. spotify:track:6yIjtVtnOBeC8SwdVHzAuF
-analysis = sp.audio_analysis("")
-features = sp.audio_features("")
-features_df = pd.DataFrame(data=features, columns=features[0].keys())
-beats_df = pd.DataFrame(data=analysis["beats"])
-segments_df = pd.DataFrame(data=analysis["segments"])
-bars_df = pd.DataFrame(data=analysis["bars"])
-tatums_df = pd.DataFrame(data=analysis["tatums"])
+def analysis(track,sp):
+    analysis = sp.audio_analysis(track)
+    features = sp.audio_features(track)
+    features_df = pd.DataFrame(data=features, columns=features[0].keys())
+    beats_df = pd.DataFrame(data=analysis["beats"])
+    segments_df = pd.DataFrame(data=analysis["segments"])
+    bars_df = pd.DataFrame(data=analysis["bars"])
+    tatums_df = pd.DataFrame(data=analysis["tatums"])
+    
+    return beats_df
 
-plt.figure(figsize=(20, 30))
-plt.xticks(rotation=90)
+if __name__ == "__main__":
+    username =  CLIENT_USERNAME
+    cid = CLIENT_ID
+    secret = CLIENT_ID
+    track = "spotify:track:6yIjtVtnOBeC8SwdVHzAuF"
+    sp = authentication(cid,secret,uri,username)
+    beats = analysis(track,sp)
+    beatStart = beats["start"]
 
-print(segments_df)
+    F = open("osutesting.osu", 'a')
+    for index, j in beats.iterrows():
+        print("256,192,",beatStart[index]*1000,",5,4,0:0:0:0:\n", sep='')
+        F.write("256,192,"+ str(beatStart[index]*1000) +",5,4,0:0:0:0:\n")
+    F.close()
