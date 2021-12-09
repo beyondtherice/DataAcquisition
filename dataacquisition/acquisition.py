27159f73175e9a6f9acf78484418ff8864656ce6
hefadog2 <>
2021-12-09 18:10:36 -0500

    Fixed variable anmes into snake case

--- dataacquisition/acquisition.py
+++ dataacquisition/acquisition.py
@@ -7,14 +7,15 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 def authentication(cid: str,secret: str,username: str) -> any:
     """Sets up the client authentication"""
-    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, 
+            client_secret=secret)
+    spot_auth = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     return spot_auth
 
 def analysis(track: str,spot_auth) -> any:
     """analyses the trck useing the spotify api"""
-    # analysis = sp.audio_analysis(track)
-    features = sp.audio_features(track)
+    analysis = spot_auth.audio_analysis(track)
+    # features = spot_auth.audio_features(track)
     # features_df = pd.DataFrame(data=features, columns=features[0].keys())
     beats_df = pd.DataFrame(data=analysis["beats"])
     # segments_df = pd.DataFrame(data=analysis["segments"])
@@ -27,8 +28,8 @@ def beatmaker(beats):
     beat_start = beats["start"]
     with open("osutesting.osu", 'a') as beatmap:
         for index, _ in beats.iterrows():
-            print("256,192,",beatStart[index]*1000,",5,4,0:0:0:0:\n", sep='')
-            beatmap.write("256,192,"+ str(beatStart[index]*1000) +",5,4,0:0:0:0:\n")
+            print("256,192,",beat_start[index]*1000,",5,4,0:0:0:0:\n", sep='')
+            beatmap.write("256,192,"+ str(beat_start[index]*1000) +",5,4,0:0:0:0:\n")
 
 def main():
     """declared variables and calls other funvtions"""
